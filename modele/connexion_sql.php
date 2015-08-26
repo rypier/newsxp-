7f17b0db76e3e3d3dbb48ae736587f01a5a7cc1a
rypier <pierrerenelherisson@gmail.com>
2015-08-26 10:10:37 +0200

    make it better

--- modele/connexion_sql.php
+++ modele/connexion_sql.php
@@ -2,7 +2,8 @@
 
 try
 {
-    $bdd = new PDO('mysql:host=localhost;port=8889;dbname=CSV_DB', 'root', 'pierry_34539763');
+    #$bdd = new PDO('mysql:host=localhost;port=8889;dbname=CSV_DB', 'root', 'pierry_34539763');
+    $bdd = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_3985968aeade495', 'b5c55c0de25058', '0e3fec64');
     //echo 'connexion reussie';
 }
 
